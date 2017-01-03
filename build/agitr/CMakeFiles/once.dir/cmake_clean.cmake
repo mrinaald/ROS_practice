808b945f416d6b34464d6cac0cc7d743f3b94241
Mrinaal Dogra <mrinaal@iitk.ac.in>
2017-01-03 16:48:09 +0530

    Added once.cpp : This program generates a single log message at each severity level.

---
+++ build/agitr/CMakeFiles/once.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/once.dir/src/once.cpp.o"
+  "/home/mrinaal/git/ROS_practice/devel/lib/agitr/once.pdb"
+  "/home/mrinaal/git/ROS_practice/devel/lib/agitr/once"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/once.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
