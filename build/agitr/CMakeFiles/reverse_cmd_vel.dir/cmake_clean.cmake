253e127395729d230587d40920addf252c0f7688
Mrinaal Dogra <mrinaal@iitk.ac.in>
2017-01-12 12:30:16 +0530

    Added the reverse.launch file which uses remapping to reverse the sense input keys.

---
+++ build/agitr/CMakeFiles/reverse_cmd_vel.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reverse_cmd_vel.dir/src/reverse_cmd_vel.cpp.o"
+  "/home/mrinaal/git/ROS_practice/devel/lib/agitr/reverse_cmd_vel.pdb"
+  "/home/mrinaal/git/ROS_practice/devel/lib/agitr/reverse_cmd_vel"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reverse_cmd_vel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/agitr/CMakeFiles/reverse_velocity.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reverse_velocity.dir/src/reverse_cmd_vel.cpp.o"
+  "/home/mrinaal/git/ROS_practice/devel/lib/agitr/reverse_velocity.pdb"
+  "/home/mrinaal/git/ROS_practice/devel/lib/agitr/reverse_velocity"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/reverse_velocity.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
