73e9b9d24428192a7473ec3f105bf310193df645
Mrinaal Dogra <mrinaal@iitk.ac.in>
2017-01-14 12:09:30 +0530

    Added set_bg_color.cpp that sets the background color of turtlesim_node using its parameters.

---
+++ build/agitr/CMakeFiles/set_bg_color.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/set_bg_color.dir/src/set_bg_color.cpp.o"
+  "/home/mrinaal/git/ROS_practice/devel/lib/agitr/set_bg_color.pdb"
+  "/home/mrinaal/git/ROS_practice/devel/lib/agitr/set_bg_color"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/set_bg_color.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
