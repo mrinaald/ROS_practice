f0cf68408039fdd4ab194fefbc0dda57bd9cfe78
Mrinaal Dogra <mrinaal@iitk.ac.in>
2017-01-15 02:16:46 +0530

    Added a program called "spawn_turtle.cpp" that calls a service.

---
+++ build/agitr/CMakeFiles/spawn_turtle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spawn_turtle.dir/src/spawn_turtle.cpp.o"
+  "/home/mrinaal/git/ROS_practice/devel/lib/agitr/spawn_turtle.pdb"
+  "/home/mrinaal/git/ROS_practice/devel/lib/agitr/spawn_turtle"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spawn_turtle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
