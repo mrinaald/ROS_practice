ce2ae360681a1c3ab8dfdb71dfc0073ca61a98fc
Mrinaal Dogra <mrinaal@iitk.ac.in>
2017-01-15 02:38:33 +0530

    Added a program "pubvel_toggle.cpp" that changes the velocity commands it publishes, based on a service that it offers.

---
+++ build/agitr/CMakeFiles/pubvel_toggle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pubvel_toggle.dir/src/pubvel_toggle.cpp.o"
+  "/home/mrinaal/git/ROS_practice/devel/lib/agitr/pubvel_toggle.pdb"
+  "/home/mrinaal/git/ROS_practice/devel/lib/agitr/pubvel_toggle"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pubvel_toggle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
