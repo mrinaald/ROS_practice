d00cd1ac1fe81b7190d7ff1cfb3ad86a2cd7abec
Mrinaal Dogra <mrinaal@iitk.ac.in>
2017-01-29 00:58:02 +0530

    Added the Histogram IP code without using any class and its launch file hist_noclass.launch

---
+++ build/agitr/CMakeFiles/hist_noclass.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hist_noclass.dir/src/hist_noclass.cpp.o"
+  "/home/mrinaal/git/ROS_practice/devel/lib/agitr/hist_noclass.pdb"
+  "/home/mrinaal/git/ROS_practice/devel/lib/agitr/hist_noclass"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hist_noclass.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
